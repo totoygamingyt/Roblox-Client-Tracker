db19576e1c3c59265e283ab449c58d33c1fde7c3
Roblox Client Tracker <rbxclientwatch@gmail.com>
2022-09-14 19:55:52 -0500

    0.545.100.5450317 (Scripts)

--- LuaPackages/AppTempCommon/Flags/getFFlagLuaAppUseVerifiedBadge.lua
+++ LuaPackages/AppTempCommon/Flags/getFFlagLuaAppUseVerifiedBadge.lua
@@ -1,5 +1,5 @@
-game:DefineFastFlag("LuaAppUseVerifiedBadgeV1", false)
+game:DefineFastFlag("LuaAppUseVerifiedBadgeV2", false)
 
 return function()
-	return game:GetFastFlag("LuaAppUseVerifiedBadgeV1")
+	return game:GetFastFlag("LuaAppUseVerifiedBadgeV2")
 end
