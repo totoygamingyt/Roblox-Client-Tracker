0de5eaed6928a6ba926951f1b217b6b48ba20bd3
Roblox Client Tracker <rbxclientwatch@gmail.com>
2022-09-14 20:31:55 -0500

    0.545.100.5450334 (Packages)

--- LuaPackages/Packages/_Index/NetworkingVirtualEvents/NetworkingVirtualEvents/requests/createUpdateVirtualEvent.lua
+++ LuaPackages/Packages/_Index/NetworkingVirtualEvents/NetworkingVirtualEvents/requests/createUpdateVirtualEvent.lua
@@ -1,29 +1,23 @@
 local NetworkingVirtualEvents = script:FindFirstAncestor("NetworkingVirtualEvents")
 
 local constants = require(NetworkingVirtualEvents.constants)
+local types = require(NetworkingVirtualEvents.types)
 
 return function(roduxNetworking)
-	type Payload = {
-		title: string,
-		description: string,
-		eventTime: {
-			startTime: DateTime,
-			endTime: DateTime,
-		},
-		universeId: number,
-	}
-
-	local UpdateVirtualEvent = roduxNetworking.POST(script, function(requestBuilder, eventId: number, payload: Payload)
-		return requestBuilder(constants.API_URL):path("v1"):path("virtual-events"):id(eventId):body({
-			title = payload.title,
-			description = payload.description,
-			eventTime = {
-				startTime = payload.eventTime.startTime:ToIsoDate(),
-				endTime = payload.eventTime.endTime:ToIsoDate(),
-			},
-			universeId = payload.universeId,
-		})
-	end)
+	local UpdateVirtualEvent = roduxNetworking.POST(
+		script,
+		function(requestBuilder, eventId: number, payload: types.UpdateVirtualEventRequest)
+			return requestBuilder(constants.API_URL):path("v1"):path("virtual-events"):id(eventId):body({
+				title = payload.title,
+				description = payload.description,
+				eventTime = {
+					startTime = payload.eventTime.startTime:ToIsoDate(),
+					endTime = payload.eventTime.endTime:ToIsoDate(),
+				},
+				universeId = payload.universeId,
+			})
+		end
+	)
 
 	return UpdateVirtualEvent
 end
--- LuaPackages/Packages/_Index/NetworkingVirtualEvents/NetworkingVirtualEvents/types.lua
+++ LuaPackages/Packages/_Index/NetworkingVirtualEvents/NetworkingVirtualEvents/types.lua
@@ -2,22 +2,60 @@ export type Config = {
 	roduxNetworking: any,
 }
 
-export type VirtualEventSchema = {
+export type EventStatus = "unpublished" | "active" | "cancelled"
+export type HostType = "user" | "group"
+
+export type EventTime = {
+	startTime: string,
+	endTime: string,
+}
+
+export type EventTimeUtc = {
+	startUtc: string,
+	endUtc: string,
+}
+
+export type Host = {
+	hostType: HostType,
+	hostId: number,
+}
+
+export type VirtualEventResponse = {
 	id: string,
 	title: string,
 	description: string,
-	eventTime: {
-		startUtc: string,
-		endUtc: string,
-	},
-	host: {
-		hostTypeId: string, -- "user" | "group"
-		hostTargetId: number,
-	},
+	eventTime: EventTimeUtc,
+	host: Host,
 	universeId: number,
-	status: string, -- "activated" | "cancelled" | "unpublished"
+	eventStatus: EventStatus,
 	createdUtc: string,
 	updatedUtc: string,
+}
+
+type PaginatedResponse<T> = {
+	nextPageCursor: string,
+	previousPageCursor: string,
+	data: { T },
+}
+
+export type PaginatedVirtualEventResponse = PaginatedResponse<VirtualEventResponse>
+
+export type CreateVirtualEventRequest = {
+	title: string,
+	description: string,
+	eventTime: EventTime,
+	universeId: number,
+}
+
+export type UpdateVirtualEventRequest = {
+	title: string?,
+	description: string?,
+	eventTime: EventTime?,
+	universeId: number?,
+}
+
+export type DeleteVirtualEventResponse = {
+	isDeleted: boolean,
 }
 
 return {}
